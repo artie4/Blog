dbc6984772d2e6e8289d60e667137287872cfcbf
Arthur <gaartur4@gmail.com>
2019-01-03 15:30:14 +0300

    edited route logic and styling

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-
+import { Link } from 'react-router-dom';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
 
@@ -9,10 +9,10 @@ class Posts extends Component {
         posts: []
     }
 
-    componentDidMount () {
+    componentDidMount() {
         console.log(this.props);
-        axios.get( '/posts' )
-            .then( response => {
+        axios.get('/posts')
+            .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
                     return {
@@ -20,9 +20,9 @@ class Posts extends Component {
                         author: 'Max'
                     }
                 });
-                this.setState({posts: updatedPosts});
+                this.setState({ posts: updatedPosts });
                 // console.log( response );
-            } )
+            })
             .catch(error => {
                 console.log(error);
                 // this.setState({error: true});
@@ -30,18 +30,23 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.setState({ selectedPostId: id });
     }
-    
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
+                return (
+                    <Link to={'/' + post.id} key={post.id}>
+                        <Post
+                            key={post.id}
+                            title={post.title}
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}
+                        />;
+                    </Link>
+                )
             });
         }
 
